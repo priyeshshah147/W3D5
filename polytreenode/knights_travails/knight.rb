b962af9efcc60fa754ca687899c76a82a0cb5595
Priyesh Shah <priyesh.shah147@gmail.com>
2021-05-10 11:34:44 -0400

    Completed Knight Travails phase 2 and phase 3

--- polytreenode/knights_travails/knight.rb
+++ polytreenode/knights_travails/knight.rb
@@ -1,26 +1,30 @@
-require_relative "\skeleton\lib\00_tree_node.rb"
+# require_relative "\skeleton\lib\00_tree_node.rb"
+require_relative "../skeleton/lib/00_tree_node.rb"
 
 class KnightPathFinder
 
+
+    attr_accessor :root_node
     def initialize(pos)
-        @start = PolyTreeNode.new(pos)
+        @start = pos
         @considered_positions = [pos]
+        build_move_tree
     end
 
     def self.valid_moves(pos)
         moves = []
         
-        moves << [pos[0] + 2, pos[1] + 1] if pos[0] < 8 && pos[1] < 8
-        moves << [pos[0] + 1, pos[1] + 2] if pos[0] < 8 && pos[1] < 8
+        moves << [pos[0] + 2, pos[1] + 1] if pos[0] + 2 < 8 && pos[1] + 1 < 8
+        moves << [pos[0] + 1, pos[1] + 2] if pos[0] + 1 < 8 && pos[1] + 2 < 8
         
-        moves << [pos[0] + 1, pos[1] - 2] if pos[0] < 8 && pos[1] >= 0
-        moves << [pos[0] + 2, pos[1] - 1] if pos[0] < 8 && pos[1] >= 0
+        moves << [pos[0] + 1, pos[1] - 2] if pos[0] + 1 < 8 && pos[1] - 2 >= 0
+        moves << [pos[0] + 2, pos[1] - 1] if pos[0] + 2 < 8 && pos[1] - 1 >= 0
         
-        moves << [pos[0] - 1, pos[1] + 2] if pos[0] >= 0 && pos[1] < 8
-        moves << [pos[0] - 2, pos[1] + 1] if pos[0] >= 0 && pos[1] < 8
+        moves << [pos[0] - 1, pos[1] + 2] if pos[0] - 1 >= 0 && pos[1] + 2 < 8
+        moves << [pos[0] - 2, pos[1] + 1] if pos[0] - 2 >= 0 && pos[1] + 1 < 8
         
-        moves << [pos[0] - 1, pos[1] - 2] if pos[0] >= 0 && pos[1] >= 0
-        moves << [pos[0] - 2, pos[1] - 1] if pos[0] >= 0 && pos[1] >= 0
+        moves << [pos[0] - 1, pos[1] - 2] if pos[0] - 1 >= 0 && pos[1] - 2 >= 0
+        moves << [pos[0] - 2, pos[1] - 1] if pos[0] - 2 >= 0 && pos[1] - 1 >= 0
 
         moves
     end
@@ -38,21 +42,33 @@ class KnightPathFinder
     end
 
     def build_move_tree
-        new_pos = []
-        while @considered_positions.length > 0
-            @considered_positions.each do |positions|
-                new_pos << new_move_positions(positions)
+        self.root_node = PolyTreeNode.new(@start)
+        queue = [self.root_node]
+        until queue.empty?
+            current_node = queue.shift
+            new_move_positions(current_node.value).each do |pos|
+                new_node = PolyTreeNode.new(pos)
+                current_node.add_child(new_node)
+                queue << new_node
             end
+        end         
+    end
+
+    def find_path(end_pos)
+        
+        trace_path_back(root_node.bfs(end_pos)) + [end_pos]
+    end
+
+    def trace_path_back(root_node)
+        pos = []
+        queue = [root_node.parent]
+        until queue[0] == nil
+            new_pos = queue.shift
+            pos.unshift(new_pos.value)
+            queue << new_pos.parent
         end
-                
+        pos
     end
-
-    # position >> 8 newposition
-    8 new_move_positions >> 8 positions
-    64
-
-    def find_path(final_pos)
-    end
-    
-
 end
+
+kpf = KnightPathFinder.new([0,0])
